b3320f089a7de89f4c540237e195c40c94e2ce01
GASTON <you@example.com>
2020-05-19 21:41:23 -0300

    added firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+var firebase = require("firebase/app");
+
+//require("firebase/auth");
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCS3ET5boFO9KQYfzXVM3wetboBTwoIPpY",
+  authDomain: "ventas-5eb5a.firebaseapp.com",
+  databaseURL: "https://ventas-5eb5a.firebaseio.com",
+  projectId: "ventas-5eb5a",
+  storageBucket: "ventas-5eb5a.appspot.com",
+  messagingSenderId: "209634879405",
+  appId: "1:209634879405:web:b8fcbc448ccd1c4c2e2324"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
 new Vue({
